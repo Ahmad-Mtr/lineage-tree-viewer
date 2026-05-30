0de850364f937c2b7f443527f4af47dc64058187
Ahmad Matar <ahmadmtr@protonmail.com>
2026-05-30 21:05:47 +0200

    add og-image

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,6 +13,13 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1, viewport-fit=cover",
       },
       { title: " حمولة آل ناصر كَرَاجَة" },
+      { property: "og:type", content: "website" },
+      { property: "og:title", content: "حمولة آل ناصر كَرَاجَة" },
+      { property: "og:image", content: "/og-image.png" },
+      { property: "og:image:width", content: "1200" },
+      { property: "og:image:height", content: "630" },
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:image", content: "/og-image.png" },
     ],
     links: [
       { rel: "stylesheet", href: appCss },
